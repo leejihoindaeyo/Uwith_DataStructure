bb69082a2eac586ffcba6f17b422e12d09f3bffd
최성민 <mococo112412@gmail.com>
2024-09-29 15:03:51 +0900

    24511.cpp

---
+++ DataStructure/2.QueueAndDeque/Group3/sungmin/24511.cpp
@@ -0,0 +1,46 @@
+//24511번: queuestack
+#include <iostream>
+#include <deque>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    deque<int> dq;
+    int n,m;
+    bool flag[100001]; //0:queue, 1:stack
+
+
+    cin>>n;
+
+    for(int i=0; i<n; i++){
+        cin>>flag[i];
+    }
+
+    for(int i=0; i<n; i++){
+
+        int x;
+        cin>>x;
+
+        if(flag[i]==0) //queue일때만 deque에 원소 삽입
+        dq.push_back(x);
+    }
+
+    cin>>m;
+
+    for(int i=0; i<m; i++){
+
+        int y;
+        cin>>y;
+        
+        dq.push_front(y);
+        cout<<dq.back()<<" ";
+        dq.pop_back();
+        
+    }
+    //전부 stack일 경우, dq에 미리 넣는 과정없이  새 dq에 push_front, pop_back과정 반복하면 됨.
+
+}
