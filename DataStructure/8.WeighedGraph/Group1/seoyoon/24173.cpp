89ab4cc4509859419a13084f435a486e2962d3c2
cr00key <rlatjdbs210@gmail.com>
2024-12-03 01:53:24 +0900

    Update 24173.cpp

--- DataStructure/8.WeighedGraph/Group1/seoyoon/24173.cpp
+++ DataStructure/8.WeighedGraph/Group1/seoyoon/24173.cpp
@@ -3,38 +3,31 @@
 
 using namespace std;
 
-// 힙 재구성 함수
 void heapify(vector<int>& arr, int n, int i) {
-    int largest = i;     // 루트를 최대값으로 설정
-    int left = 2 * i + 1; // 왼쪽 자식
-    int right = 2 * i + 2; // 오른쪽 자식
-
-    // 왼쪽 자식이 루트보다 크다면
+    int largest = i;    
+    int left = 2 * i + 1; 
+    int right = 2 * i + 2; 
+    
     if (left < n && arr[left] > arr[largest])
         largest = left;
 
-    // 오른쪽 자식이 현재 최대값보다 크다면
     if (right < n && arr[right] > arr[largest])
         largest = right;
 
-    // 최대값이 루트가 아니라면 교환
     if (largest != i) {
         swap(arr[i], arr[largest]);
-        heapify(arr, n, largest); // 재귀적으로 힙 재구성
+        heapify(arr, n, largest);
     }
 }
 
-// 힙 정렬
 void heapSort(vector<int>& arr, int n) {
-    // 힙 구성
     for (int i = n / 2 - 1; i >= 0; i--) {
         heapify(arr, n, i);
     }
 
-    // 힙 정렬 과정
     for (int i = n - 1; i > 0; i--) {
-        swap(arr[0], arr[i]);    // 루트 노드와 마지막 노드 교환
-        heapify(arr, i, 0);      // 남은 요소로 힙 재구성
+        swap(arr[0], arr[i]);   
+        heapify(arr, i, 0);      
     }
 }
 
